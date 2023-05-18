81cdb3e35ed53977eed61c71f5a1d46ffb05b7c8
Kat11 <sadaf.rs1194@gmail.com>
2023-05-18 14:41:02 +0430

    Commit

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from time import sleep
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+from pathlib import Path
+import os
 
 driver = webdriver.Chrome(executable_path="C:/chromedriver.exe")
 # driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
@@ -21,13 +23,20 @@ driver.switch_to.new_window('window')
 driver.get("https://www.wikipedia.com")
 print(driver.title)
 # driver.refresh()
-sleep(2)
+sleep(1)
 Wikipedia_handle = driver.current_window_handle
 print('Wikipedia_handle' + str(Wikipedia_handle))
-sleep(2)
-driver.switch_to.window(driver.window_handles[0])
-sleep(2)
+sleep(1)
+# driver.switch_to.window(driver.window_handles[0])
+# sleep(2)
+driver.maximize_window()
+sleep(1)
 
+Current_path = Path(__file__).parent
+file_name = os.path.join(str(Current_path), 'Session.png')
+
+driver.save_screenshot("file_name")
+sleep(2)
 # driver.find_element('name', 'q').send_keys("wikipedia")
 # sleep(5)
 # driver.find_element('name', 'q').send_keys(Keys.RETURN)
